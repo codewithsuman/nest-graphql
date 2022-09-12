b70ee8bc93d5d9c4c20cccaed4863c0016ebc267
Suman Jha <meetsuman@icloud.com>
2022-09-12 18:21:52 -0400

    Added fastify

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { NestGraphModule } from './nest-graph.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    NestGraphModule,
+    new FastifyAdapter({ logger: true }),
+  );
   await app.listen(3000);
 }
 bootstrap();
