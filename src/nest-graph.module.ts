e6d18aa0e940ba6251d1c7d809a4245ec3349f7b
Suman Jha <meetsuman@icloud.com>
2022-09-12 18:27:41 -0400

    Added Graph dependency

--- src/nest-graph.module.ts
+++ src/nest-graph.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { MercuriusDriver, MercuriusDriverConfig } from '@nestjs/mercurius';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRootAsync<MercuriusDriverConfig>({
+      driver: MercuriusDriver,
+      useFactory: async () => ({
+        graphiql: true,
+        autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+        allowBatchedQueries: true,
+        sortSchema: true,
+        cache: false,
+        context: (request, reply) => ({
+          randomValue: Math.random(),
+        }),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
